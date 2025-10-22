00e50a22a909846a94a9b6579769453cf2a0f057
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-10-22 18:02:25 +0600

    refactor(script): Improve module generation script service template

--- script/generate-module.js
+++ script/generate-module.js
@@ -68,23 +68,16 @@ export const ${camelName}: Router = express.Router();
 const generateService = (_moduleName) => {
   return `
 import type { RequestHandler } from "express";
+import { sendInternalError, sendSuccess } from "@/helpers";
 
 // TODO: Add your request handlers here
 // Example:
  export const handler: RequestHandler = async (req, res) => {
    try {
-     res.status(200).json({
-       status: 200,
-       message: "Success",
-       data: null,
-     });
+		return sendSuccess(res, "Success", null);
    } catch (error) {
      console.log(error);
-     res.status(500).json({
-       status: 500,
-       message: "Internal Server Error",
-       data: null,
-     });
+		return sendInternalError(res, "Internal Server Error");
    }
  };
 `;
