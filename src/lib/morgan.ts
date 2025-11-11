32ff641380f178369ab7342f84dde90aa1d7cf76
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-11-11 12:14:19 +0600

    fix: docker timezone

--- src/lib/morgan.ts
+++ src/lib/morgan.ts
@@ -6,9 +6,10 @@ morgan.token("time", () => {
 	const now = new Date();
 	const hours = now.getHours();
 	const minutes = now.getMinutes().toString().padStart(2, "0");
-	const suffix = hours >= 12 ? "PM" : "AM";
-	const formattedHour = hours % 12 || 12;
-	return `${formattedHour}:${minutes}${suffix}`;
+	const seconds = now.getSeconds().toString().padStart(2, "0");
+  const suffix = hours >= 12 ? "PM" : "AM";
+  const formattedHour = hours % 12 || 12;
+  return `${formattedHour}:${minutes}:${seconds}${suffix}`;
 });
 
 // Define your custom format
