f7fe1b61e2c92f8207e2de290aae552b6af4e793
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-11-13 11:50:10 +0600

    feat: Implemented and  updated models for payment and offer management

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import { Category } from "./models/category.model";
 import { Certification } from "./models/certification.model";
 import { Conversation } from "./models/conversation.model";
-
 import { Experience } from "./models/experience.model";
 import { FcmToken } from "./models/fcm-token.model";
 import { JobApplicationRequest } from "./models/job-application-request.model";
@@ -9,22 +8,29 @@ import { Job } from "./models/job.model";
 import { Location } from "./models/location.model";
 import { Message } from "./models/message.model";
 import { Notification } from "./models/notification.model";
+import { Offer } from "./models/offer.model";
 import { Review } from "./models/review.model";
+import { Transaction } from "./models/transaction.model";
 import { User } from "./models/user.model";
+import { Wallet } from "./models/wallet.model";
 import { WorkSample } from "./models/work-samples.model";
 
 export const db = {
-	user: User,
-	category: Category,
-	job: Job,
-	review: Review,
-	location: Location,
-	jobApplicationRequest: JobApplicationRequest,
-	notification: Notification,
-	fcmToken: FcmToken,
-	conversation: Conversation,
-	message: Message,
-	experience: Experience,
-	workSample: WorkSample,
-	certification: Certification,
+  user: User,
+  category: Category,
+  job: Job,
+  review: Review,
+  location: Location,
+  jobApplicationRequest: JobApplicationRequest,
+  notification: Notification,
+  fcmToken: FcmToken,
+  conversation: Conversation,
+  message: Message,
+  experience: Experience,
+  workSample: WorkSample,
+  certification: Certification,
+  // Payment system models
+  offer: Offer,
+  wallet: Wallet,
+  transaction: Transaction,
 };
